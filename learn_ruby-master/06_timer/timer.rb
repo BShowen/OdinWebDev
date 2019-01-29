47402a7bace6bf183f3bffb45de631d3bdf3f4ca
Tyler Devers <devers.tyler@gmail.com>
2019-01-29 14:24:53 -0800

    project ruby - working on time_string, account for minutes

--- learn_ruby-master/06_timer/timer.rb
+++ learn_ruby-master/06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
-  #write your code here
+  
+  attr_accessor :seconds, :time_string
+
+  def initialize 
+    @seconds = 0
+  end
+  
+  def time_string
+    if (@seconds < 10)
+      @time_string = "00:00:0#{@seconds}"
+    else
+      @time_string = "00:00:#{@seconds}"
+    end
+  end
+  
+
 end
