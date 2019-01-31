919815ff5b793a0500601b4f7a72d35b7f686a34
Tyler Devers <devers.tyler@gmail.com>
2019-01-30 22:01:38 -0800

    blogger - added a few gems

--- blogger/blogger/config/routes.rb
+++ blogger/blogger/config/routes.rb
@@ -6,3 +6,12 @@ Rails.application.routes.draw do
   end
 end
   
+Blogger::Application.routes.draw do
+
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
+  resources :tags
+
+end
