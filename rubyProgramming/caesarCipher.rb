535f1da68624bdb98db939268c437c90a7d73876
Tyler Devers <devers.tyler@gmail.com>
2019-02-03 13:27:18 -0800

    ruby programming - implemented caesar cipher

--- rubyProgramming/caesarCipher.rb
+++ rubyProgramming/caesarCipher.rb
@@ -1,10 +1,28 @@
 def caesar_cipher(string, shift)
-    encoded = ""
-    string.each_char { |letter| 
-        if letter.match(/[a-z]/)
-            puts letter
-        end 
+    result = ''
+
+    # check each string character, if its a letter then increment it, else return the 
+    # return the original character
+    string.each_char { |char| 
+        if char.match?(/[a-zA-Z]/)
+            counter = 0
+            while counter < shift
+                if char == "z"
+                    char = "a"
+                elsif char == "Z"
+                    char = "A"
+                else
+                    char.next!
+                end
+                counter += 1
+            end
+            result += char
+        else
+            result += char
+        end
     }
+    
+    print result + "\n"
 end
 
-caesar_cipher("stop", 4)
+caesar_cipher("work with !specials??", 4)
