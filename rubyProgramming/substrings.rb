64069582d5042f48e1413628aa93eb844d714e38
Tyler Devers <devers.tyler@gmail.com>
2019-02-04 14:20:08 -0800

    project ruby - completed substrings.rb

--- rubyProgramming/substrings.rb
+++ rubyProgramming/substrings.rb
@@ -1,8 +1,27 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substring()
+def substrings(phrase, list)
+    phrase.downcase!
+    word_array = phrase.split(" ")
+    result = Hash.new(0)
 
+    word_array.each { |word| 
+    
+        list.each { |element|
+            if (word.include?(element))
+                result[element] += 1
+            else
+                # puts "doesn't"
+            end
+            
+        }
+    }
+
+    print result
+    print "\n"
+    return result
 end
 
 
 substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
