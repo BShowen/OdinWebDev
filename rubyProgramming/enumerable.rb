916357554bdefdc0f7ee25de3fdbe7de580c2be1
Tyler Devers <devers.tyler@gmail.com>
2019-02-05 14:18:06 -0800

    advanced building blocks - began inject/reduce method

--- rubyProgramming/enumerable.rb
+++ rubyProgramming/enumerable.rb
@@ -18,12 +18,105 @@ module Enumerable
         end
         self
     end
+
+    def my_select
+        result = Array.new
+        index = 0
+        while index < self.length
+            element = self[index]
+            result << element if yield(element) 
+            index += 1
+        end
+        result
+    end
+
+    def my_all?
+        index = 0
+        while index < self.length
+            element = self[index]
+            return false if !yield(element) 
+            index += 1
+        end
+        true
+    end
+
+    def my_any?
+        index = 0
+        while index < self.length
+            element = self[index]
+            if yield(element)
+                return true
+            end
+            index += 1
+        end
+        false
+    end
+
+    def my_none?
+        index = 0
+        while index < self.length
+            element = self[index]
+            if yield(element)
+                return false
+            end
+            index += 1
+        end
+        true
+    end
+
+    def my_count(matches = nil)
+        count = 0
+        if block_given?
+            self.my_each { |x|
+                count += 1 if yield(x)
+            }
+            return count
+        end
+        if matches
+            self.my_each { |x| count += 1 if x == matches}
+        else
+            self.my_each { |x| count += 1 }
+        end
+        count
+    end
+
+    def my_map
+        list = self.to_a
+        result = Array.new
+        list.my_each { |element| 
+            result << yield(element)
+        }
+        result
+    end
+
+    def my_inject
+        
+    end
+
 end
 
-given_array = %w[here it is]
+# %w[here it is].my_each { |x| print x += " now", "--" }
+# %w[here it is].each { |x| puts x += " now", "--" }
 
-# given_array.my_each { |x| print x += " now", "--" }
-# given_array.each { |x| puts x += " now", "--" }
+# p %w[here it is].my_each_with_index { |x, i| print "#{x} is element #{i}"}
+# p %w[here it is].each_with_index { |x, i| print "#{x} is element #{i}"}
 
-p given_array.my_each_with_index { |x, i| print "#{x} is element #{i}"}
-p given_array.each_with_index { |x, i| print "#{x} is element #{i}"}
+# p [1,2,3,4,5].my_select { |x| x.even? }
+# p [1,2,3,4,5].select { |x| x.even? }
+
+# p %w[at bear cat].my_all? { |word| word.length >= 3 }
+# p %w[at bear cat].all? { |word| word.length >= 3 }
+
+# p %w[ant bear cat].my_any? { |word| word.length >= 4 }
+# p %w[ant bear cat].any? { |word| word.length >= 4 }
+
+# p %w{ant bear cat}.my_none? { |word| word.length == 5 }
+# p %w{ant bear cat}.none? { |word| word.length == 5 }
+
+# p [1,2,3,4,2,5,6,2].my_count
+# p [1,2,3,4,2,5,6,2].my_count(2)
+# p [1,2,3,4,2,5,6,2].my_count { |x| x%2==0 }
+
+# p (1..4).my_map { |i| i*i }
+# p (1..4).my_map { "cat"  }
+
