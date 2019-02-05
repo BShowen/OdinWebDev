cf85b1e083677f59b4235a7d67dc6905d8ccaaa5
Tyler Devers <devers.tyler@gmail.com>
2019-02-05 13:10:35 -0800

    advanced building blocks - completed my_each_with_index

--- rubyProgramming/enumerable.rb
+++ rubyProgramming/enumerable.rb
@@ -9,9 +9,21 @@ module Enumerable
         self
     end
     
+    def my_each_with_index
+        index = 0
+        while index < self.length
+            element = self[index]
+            yield(element, index)
+            index += 1
+        end
+        self
+    end
 end
 
 given_array = %w[here it is]
-# p given_array.each
-p given_array.my_each { |x| puts x += " now" }
-# p given_array.each { |x| puts x += " now" }
+
+# given_array.my_each { |x| print x += " now", "--" }
+# given_array.each { |x| puts x += " now", "--" }
+
+p given_array.my_each_with_index { |x, i| print "#{x} is element #{i}"}
+p given_array.each_with_index { |x, i| print "#{x} is element #{i}"}
