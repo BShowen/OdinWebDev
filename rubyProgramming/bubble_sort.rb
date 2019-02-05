9c620084d26eb5e2f3fa4274dd65006eaa2e1057
Tyler Devers <devers.tyler@gmail.com>
2019-02-05 11:12:48 -0800

    advanced building blocks - completed bubble_sort_by method

--- rubyProgramming/bubble_sort.rb
+++ rubyProgramming/bubble_sort.rb
@@ -11,12 +11,28 @@ def bubble_sort(array)
         }
     }
 
-    print array
-    print "\n"
     return array
 
 end
 
+def bubble_sort_by(array)
+    
+    array.length.times { 
+        array.each_index { |i|
+            if i == 0
+                # skip first array value
+            else
+                block = yield(array[i-1], array[i])
+                # puts "block value #{block}"
+                if block > 0
+                    array[i-1], array[i] = array[i], array[i-1]
+                end
+            end
+        }
+    }
 
-bubble_sort([9,8,7,6,5,4,3,2,1])
-bubble_sort([4,3,78,2,0,2])
+    return array
+end
+
+p bubble_sort([9,8,7,6,5,4,3,2,1]) 
+p bubble_sort_by(["hi","hello","hey"]) { |left, right| left.length - right.length }
