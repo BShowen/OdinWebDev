49b7cc9dcf90f6dab37af52e712cbbdc79f39353
Tyler Devers <devers.tyler@gmail.com>
2019-02-10 08:04:19 -0800

    master_mind project - implemented getPlayerGuess and checkGuessAgainstCode - TODO fine tune them

--- rubyProgramming/master_mind/game.rb
+++ rubyProgramming/master_mind/game.rb
@@ -1,11 +1,10 @@
 class Game
 
-    attr_accessor(:code)
+    attr_accessor(:code, :guess)
 
     def initialize 
-
         @code = generate_code
-
+        @guess = []
     end
 
     def generate_code
@@ -18,7 +17,26 @@ class Game
         code
     end
 
+    def get_players_guess
+        puts "input your guess. It must be 4 integers long, between 1 and 6."
+        guess = gets.chomp
+        guess.each_char { |item|
+            @guess << item.to_i
+        }
+    end
+
+    def check_guess_against_code
+        for num in @guess
+            if @code.include? num
+                p true
+            else
+                p false
+            end
+        end
+    end
 end
 
 game = Game.new
 p game.code
+game.get_players_guess
+game.check_guess_against_code
