78e06e71ed3c6418ed28e00b42fb4568c123d173
Tyler Devers <devers.tyler@gmail.com>
2019-02-12 20:23:04 -0800

    hangman - random_word() method working

--- rubyProgramming/hangman/game.rb
+++ rubyProgramming/hangman/game.rb
@@ -2,3 +2,16 @@
 
 dictionary = File.read "5desk.txt"
 dict = dictionary.split()
+approved_words = []
+
+dict.each do |word|
+    if word.length >5 && word.length <= 12
+        approved_words << word
+    end
+end
+
+def random_word(list)
+    return list[rand(0...list.length)]
+end
+
+p random_word(approved_words)
